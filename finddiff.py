2c63f48a0519211c3ba86eb7d2b1cfb44c806fc2
febinstephen <godsonfebin@gmail.com>
2016-05-20 10:08:10 +0530

    finddiff return diffs as string used subprocess

--- finddiff.py
+++ finddiff.py
@@ -1,44 +1,12 @@
-import sh
+from sh import git
 import os
 from tempfile import NamedTemporaryFile, mkdtemp
 
-"""
-def make_a_file_in_a_dir(temp_dir):
-    sh.tar("cd", temp_dir)
-    sh.tar("git", "init")
-    temp_file = NamedTemporaryFile(delete=True, dir=".")
-    return temp_file
-"""
-
 
 def find_diffs(input_file1, input_file2):
-    if os.path.exists(input_file1) and os.path.exists(input_file2):
-        try:
-            temp_dir = mkdtemp(prefix="gittemp", dir=os.getcwd())
-            #temp_file = make_a_file_in_a_dir(temp_dir)
-            print"----------------"
-            print temp_dir
-            sh.cd(temp_dir)
-            print(sh.pwd())
-            sh.git.init()
-            temp_file = NamedTemporaryFile(delete=False, dir=".")
-            print"----------------"
-            print(sh.ls("-al"))
-            print"----------------"
-            #either spawn a subprocess if needed
-            #temp_file.write(input_file1.read())
-            temp_file.write("hello world")
-            print(temp_file.read())
-            sh.git.add(".")
-            temp_file.write("hello india")
-            print(temp_file.read())
-            print sh.git.diff()
-            #tempf.seek(0)
-            #print(tempf.read())
-            #tempf.close()
-        finally:
-            os.rmdir(temp_dir)
-    else:
-        print"File dosen't exist"
-
+    import subprocess
+    p = subprocess.Popen(['git', 'diff', "--no-index", input_file1, input_file2], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    print out
+    print type(out)
 find_diffs("hello1.txt", "hello2.txt")
