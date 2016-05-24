448e8976177c23079b9b36f29b509af7b94852ca
febinstephen <godsonfebin@gmail.com>
2016-05-24 19:25:12 +0530

    added two functions find_diffs and difftohtml

---
+++ gitdiffmatch/utils.py
@@ -0,0 +1,21 @@
+def find_diffs(input_file1, input_file2):
+    """
+    takes two --files-- as inputs and returns
+    diffs b/w the files as a 'string'
+    """
+    import subprocess
+    p = subprocess.Popen(['git', 'diff', "--no-index", input_file1, input_file2],
+    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    f = open("diffs", 'w')
+    f.write(out)
+    return out
+
+
+def difftohtml(input_file):
+    f = open(input_file, 'r')
+    for line in f.readlines():
+        if line[0] == '-':
+            print '<del>' + line + '</del>' + '\n'
+        elif line[0] == '+':
+            print '<ins>' + line + '</ins>' + '\n'
--- gitdiffmatch/views.py
+++ gitdiffmatch/views.py
@@ -1,16 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-def find_diffs(input_file1, input_file2):
-    """
-    takes two --files-- as inputs and returns
-    diffs b/w the files as a 'string'
-    """
-    import subprocess
-    p = subprocess.Popen(['git', 'diff', "--no-index", input_file1, input_file2],
-    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = p.communicate()
-    f = open("diffs", 'w')
-    f.write(out)
-    return out
