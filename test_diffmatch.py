f30410706bc042bd86d8025550a3b8046c11346e
febinstephen <godsonfebin@gmail.com>
2016-05-27 21:52:15 +0530

    removed some unnecessary files

--- test_diffmatch.py
+++
@@ -1,6 +0,0 @@
-from diff_match_patch import diff_match_patch
-dmp = diff_match_patch()
-diffs = dmp.diff_main('Good dog', 'Bad dog')
-print diffs
-print dmp.diff_cleanupSemantic(diffs)
-print dmp.diff_prettyHtml(diffs)
