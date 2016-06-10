13392c6b27fa3d7782ca4c8db592ab9e58f16da0
febinstephen <godsonfebin@gmail.com>
2016-06-10 20:43:09 +0530

    added a new fun in utils

--- gitdiffmatch/utils.py
+++ gitdiffmatch/utils.py
@@ -1,19 +1,33 @@
-def find_diffs(input_file1, input_file2):
+def finddiffs_of_two_files(input_file1, input_file2):
     """
     takes two --files-- as inputs and returns
     diffs b/w the files as a 'string'
     """
     import subprocess
-    p = subprocess.Popen(['git', 'diff', "--no-index", input_file1, input_file2],
+    import sh
+    p = subprocess.Popen(['git', 'diff', "--color", "--no-index", "--word-diff=plain", input_file1, input_file2],
     stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
-    diffs = out.split("\n")
-    diffs_list = list()
-    for line in diffs:
-        if line.startswith('-'):
-            diffs_list.append(line)
-        if line.startswith('+'):
-            diffs_list.append(line)
-    return diffs_list
+    return out
 
-#print find_diffs("hello1.txt", "hello2.txt")
+
+def finddiffs_of_two_strings(str1, str2):
+    import tempfile
+    tempone = tempfile.NamedTemporaryFile(suffix='_suffix', prefix='prefix_', dir='.',)
+    temptwo = tempfile.NamedTemporaryFile(suffix='_suffix', prefix='prefix_', dir='.',)
+    try:
+        stringone = ""
+        stringtwo = ""
+        tempone.write(str1)
+        tempone.seek(0)
+        temptwo.write(str2)
+        temptwo.seek(0)
+        stringone = str(tempone.name)
+        stringtwo = str(temptwo.name)
+        return finddiffs_of_two_files(stringone, stringtwo)
+    finally:
+        tempone.close()
+        temptwo.close()
+
+#print finddiffs_of_two_strings("some data", "some stuff")
+#print finddiffs_of_two_files("hello1.txt", "hello2.txt")
--- gitdiffmatch/views.py
+++ gitdiffmatch/views.py
@@ -2,14 +2,13 @@ from django.shortcuts import render
 from . import utils
 from django.http import HttpResponse
 from django.template import loader
-from django.core.urlresolvers import reverse
 
 
 def index(request):
-    #return HttpResponse("Hello, World. You're at the polls index.")
-    diffs = utils.find_diffs("hello1.txt", "hello2.txt")
+    diffs = utils.finddiffs_of_two_files("hello1.txt", "hello2.txt").split('\n')
+    #diffs = utils.finddiffs_of_two_strings("i am not dump", "i was not hero").split('\n')
     template = loader.get_template('gitdiffmatch/index.html')
     context = {
-        'diffs': diffs,
+        'diff': diffs,
     }
     return HttpResponse(template.render(context, request))
